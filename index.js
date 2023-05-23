6a251f1e024856791c1cdb40a65fd7c9b307459e
Romain Travers <romain.travers@beyond-the-sea.fr>
2023-05-23 17:52:51 +0200

    commit

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const app = express();
+const puppeteer = require('puppeteer');
 
-const PORT = process.env.PORT || 4000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
+async function getData() {
+ 
+}
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.get('/', async (req, res) => {
+  const browser = await puppeteer.launch({headless: 'new',
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.windguru.cz/station/2748');
+  await page.waitForTimeout(1000);
+  await page.waitForSelector('.wgs_wind_avg_value')
+  //await page.waitForFunction('document.getElementsByClassName("wgs_wind_avg_value")[0].childNodes[0].length > 0')
+  const data = await page.evaluate(() => {
+    return document.querySelector('.wgs_wind_avg_value').innerText;
+  });
+  //console.log(data);
+  await browser.close();
+  res.json({wind: data})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
